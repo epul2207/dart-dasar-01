34a071460ea282c3053d948895f151aeae818bb8
epul2207 <epuls073@gmail.com>
2023-09-03 18:52:47 +0700

    Menambahkan file final

---
+++ final_04.dart
@@ -0,0 +1,20 @@
+void main() {
+  print('Kata kunci final');
+
+//   Kata Kunci final
+// ● Secara default, variable di Dart bisa dideklarasikan ulang, artinya jika sebelumnya kita membuat
+// variable name dengan value “Eko”, kita bisa ubah variable tersebut menjadi “Joko” dengan cara
+// namaVariable = “Joko”
+// ● Kadang ada kasus dimana kita tidak ingin sebuah variable bisa dideklarasikan ulang, untuk
+// melakukan itu kita bisa gunakan kata kunci final :
+// final TipeData namaVariable = value;
+// final namaVariable = value;
+
+  //final TipeData namaVariable = value;
+
+  final String agama = "Islam";
+
+  final nama = 'Otong';
+
+  print('nama saya $nama, agama saya $agama');
+}
