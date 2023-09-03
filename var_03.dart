336e257d61daf176f59a76882a1f3fd43e6e1f84
epul2207 <epuls073@gmail.com>
2023-09-03 18:49:06 +0700

    Menambahkan file var

---
+++ var_03.dart
@@ -0,0 +1,18 @@
+void main() {
+  print('Kata kunci var = salah satu tipe data');
+
+//   Kata Kunci var
+// ● Saat kita membuat variable langsung dengan nilainya, kita bisa menggunakan kata kunci var
+// sebagai pengganti TipeData nya
+// ● Ini mirip dengan bahasa pemrograman seperti Java, Go-Lang, Kotlin dan lain-lain
+// ● TipeData akan dibaca sesuai dengan isi nilai nya secara otomatis oleh Dart, sehingga kita tidak
+// perlu menyebutkan TipeData nya lagi
+// ● Cara menggunakan kata kunci var, seperti ini :
+// var namaVariable = value;
+
+  //var namaVariable = value;
+
+  int nilai_matematika = 98;
+
+  print('aku mendapatkan nilai matematika $nilai_matematika');
+}
