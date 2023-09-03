7ad1334175d033487e43bf26d1269225441bbae9
epul2207 <epuls073@gmail.com>
2023-09-03 19:33:35 +0700

    Menambahkan file komentar

---
+++ komentar_07.dart
@@ -0,0 +1,19 @@
+void main() {
+  print('Komentar');
+
+//   Komentar
+// ● Komentar adalah kode program yang akan diabaikan ketika kode program di compile atau
+// dijalankan
+// ● Biasanya komentar digunakan untuk menambahkan dokumentasi pada kode program
+// ● Di Dart, ada banyak jenis komentar
+
+// Jenis Komentar
+// ● Single-line atau satu baris, bisa menggunakan perintah :
+// // silahkan masukkan komentar disini
+// ● Multi-line atau lebih dari satu baris, bisa menggunakan perintah :
+// /*
+// isi komentar
+// */
+// ● Documentation, ini adalah jenis komentar yang biasanya digunakan sebagai dart documentation :
+// /// isi dokumentasi
+}
