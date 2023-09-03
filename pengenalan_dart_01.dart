c61e290dd573fbad27e624b4d282c87950c624b2
epul2207 <epuls073@gmail.com>
2023-09-03 14:32:20 +0700

    Menambahkan isi pengenalan dart

--- pengenalan_dart_01.dart
+++ pengenalan_dart_01.dart
@@ -1,3 +1,78 @@
 void main() {
   print('Ini adalah pengenalan tentang dart dan cara menginstalan sdk dart');
+
+// Dart merupakan bahasa pemrograman baru yang pertama kali diperkenalkan tahun 2011
+// ● Dart dibuat di Google oleh Lars Bak dan Kasper Lund
+// ● Dart merupakan bahasa pemrograman yang awal nya di buat untuk Frontend (web dan mobile),
+// namun Dart juga sebenarnya bisa digunakan untuk Backend, namun memang lebih populer
+// penggunaannya di Frontend
+// ● Dart merupakan bahasa pemrograman yang Open Source
+// ● https://dart.dev/
+
+// Dart Software Development Kit
+// ● Dart merupakan bahasa pemrograman yang multi platform, artinya bisa di install di berbagai
+// sistem operasi, seperti Windows, Linux dan Mac
+// ● Untuk membuat aplikasi menggunakan bahasa pemrograman Dart, kita membutuhkan Dart SDK
+// (Software Development Kit)
+// ● SDK ini digunakan untuk melakukan kompilasi kode program Dart yang kita buat dan juga
+// menjalankan kode program Dart yang kita buat
+
+// Menginstall Dart SDK
+// ● Untuk menginstall Dart SDK, kita bisa download aplikasi Dart SDK sesuai sistem operasi yang kita
+// gunakan
+// ● https://dart.dev/get-dart
+
+// Text Editor
+// ● Untuk membuat kode program Dart, kita membutuhkan Text Editor yang bagus, terdapat beberapa
+// aplikasi yang populer untuk membuat kode program Dart
+// ● Visual Studio Code : https://code.visualstudio.com/
+// ● IntelliJ IDEA : https://www.jetbrains.com/idea/
+// ● Android Studio : https://developer.android.com/studio
+
+// Dart SDK
+// ● Saat kita menginstall Dart SDK, Dart memiliki banyak sekali executable file yang bisa diakses via
+// terminal / command line
+// ● Aplikasi tersebut bisa digunakan untuk membuat project, melakukan kompilasi sampai
+// menjalankan kode program
+// ● Banyak aplikasi yang terdapat di dalam Dart SDK, yang akan kita pelajari secara bertahap
+// ● https://dart.dev/get-dart
+
+// Membuat Project
+// ● Untuk membuat project Dart, kita bisa gunakan perintah :
+// dart create nama_project
+
+// Membuka Project
+// ● Saat membuat project, secara otomatis akan dibuat folder baru dengan nama sesuai dengan nama
+// projectnya
+// ● Selanjutnya, silahkan buka project tersebut menggunakan Text Editor yang kita gunakan
+
+// Program Hello World
+// ● Saat belajar bahasa pemrograman, biasanya hal yang pertama dilakukan adalah membuat aplikasi
+// Hello World
+// ● Aplikasi Hello World adalah aplikasi sederhana yang menampilkan tulisan Hello World
+// ● Kode program Dart, disimpan dalam file dengan file extension .dart
+// ● Dart mirip dengan bahasa pemrograman Java, setiap akhir statement dalam kode program, diakhiri
+// dengan karakter titik koma.
+
+// Dart Main Function
+// ● Dart hampir mirip dengan bahasa pemrograman seperti C, Java, Kotlin, atau Go-Lang
+// ● Saat kita membuat kode program Dart, kita harus membuat main function terlebih dahulu
+// ● Main function merupakan fungsi utama yang akan dieksekusi oleh Dart
+// Kode : Dart Main Function
+
+// Function print
+// ● Untuk menampilkan tulisan di Dart, kita bisa menggunakan function print
+// ● print memiliki parameter data string / text, dimana data tersebut bisa menggunakan kutip satu
+// atau kutip dua
+// ● Misal jika kita ingin menampilkan tulisan : Hello World, kita bisa membuat kode program
+// print(‘Hello World’) atau print(“Hello World”)
+// Kode : Hello World
+
+// Menjalankan Kode Program Dart
+// ● Untuk menjalankan kode program yang sudah kita buat, kita bisa menggunakan executable file
+// dart, caranya dengan cara menggunakan perintah run, lalu diikuti lokasi file dart yang ingin kita
+// eksekusi, misal
+// ● dart run bin/hello_world.dart
+
+//ini adalah alur belajar dari porgrammer zaman now pa eko
 }
