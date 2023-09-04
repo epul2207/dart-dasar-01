53248df494f9ce3dae745bc1392ab11802d27c6e
epul2207 <epuls073@gmail.com>
2023-09-04 10:28:51 +0700

    Menambahkan file dynamic

---
+++ dynamic_11.dart
@@ -0,0 +1,15 @@
+// Dynamic
+// ● Kadang ada kebutuhan kita ingin membuat variable yang bisa menampung semua jenis tipe data
+// ● Pada kasus ini, kita bisa menggunakan tipe data dynamic
+
+void main() {
+  print('Tipe Data Dynamic');
+
+  dynamic data = 'Otong';
+
+  print(data);
+
+  data = 100;
+
+  print(data);
+}
