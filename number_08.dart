c106de8f6b48c4f9c196ef8f21f4bb893db4684e
epul2207 <epuls073@gmail.com>
2023-09-04 09:00:27 +0700

    Menambahkan file number

---
+++ number_08.dart
@@ -0,0 +1,16 @@
+void main() {
+  print('Tipe Data Number');
+
+//   Number
+// ● Number adalah tipe data angka, terdapat dua jenis tipe data Number, int dan double
+// ● int adalah tipe data bilangan bulat
+// ● double adalah tipe data bilangan desimal
+// ● Penulisan koma dalam double menggunakan titik, bukan koma, jadi jika kita akan membuat
+// bilangan desimal 0,5 (nol koma lima), maka ditulis 0.5
+
+  int nilai_bulat = 100;
+  double nilai_desimal = 98.09;
+
+  print(nilai_bulat);
+  print(nilai_desimal);
+}
