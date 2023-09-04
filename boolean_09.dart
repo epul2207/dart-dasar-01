c380480fadf19f2f4f4c704b8ed892fcece8af4b
epul2207 <epuls073@gmail.com>
2023-09-04 10:12:20 +0700

    Menambahkan file boolean

---
+++ boolean_09.dart
@@ -0,0 +1,17 @@
+void main() {
+  print('tipe data Boolean');
+
+//   Boolean
+// ● Boolean adalah tipe data yang hanya memiliki dua nilai, yaitu benar atau salah
+// ● Boolean direpresentasikan dengan kata kunci bool
+// ● Untuk nilai benar, menggunakan kata kunci true
+// ● Untuk nilai salah, menggunakan kata kunci false
+
+  bool nilai = true;
+
+  print(nilai);
+
+  nilai = false;
+
+  print(nilai);
+}
