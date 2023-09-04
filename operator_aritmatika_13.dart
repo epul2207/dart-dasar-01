d713441414cb1166ae8faf339361ada4376fea83
epul2207 <epuls073@gmail.com>
2023-09-04 10:51:41 +0700

    Menambahkan file aritmatika

---
+++ operator_aritmatika_13.dart
@@ -0,0 +1,42 @@
+// Operator Aritmatika
+// Operator Keterangan
+// + Pertambahan
+// - , -expression Pengurangan atau Negatif
+// * Perkalian
+// / Pembagian, Hasil double
+// ~/ Pembagian, Hasil int
+// % Sisa Bagi
+
+void main() {
+  print('Operator Aritmatika');
+
+  int a = 10;
+  int b = 11;
+
+  //pertambahan
+  var c = a + b;
+
+  //pengurangan
+  var d = b - a;
+
+  //perkalian
+  var e = a * b;
+
+  //pembagian hasil double
+  var f = b / a;
+
+  //pembagian hasil int
+
+  var g = b ~/ a;
+
+  //sisa bagi atau modulus
+
+  var h = b % a;
+
+  print(c);
+  print(d);
+  print(e);
+  print(f);
+  print(g);
+  print(h);
+}
