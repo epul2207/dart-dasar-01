1ab604d94f0bdd093514346b58878a2acbabbd9f
epul2207 <epuls073@gmail.com>
2023-09-04 19:31:34 +0700

    Menambahkan file operator perbandingan

---
+++ operator_perbandingan_14.dart
@@ -0,0 +1,33 @@
+void main() {
+  print('Operator perbandingan');
+
+//   Operator Perbandingan
+// ● Operator perbandingan adalah operator yang hasilnya adalah bool
+// ● Operator perbandingan bisa digunakan di tipe data number dan string
+
+// Operator Perbandingan
+// Operator Keterangan
+// == Sama dengan
+// != Tidak sama dengan
+// > Lebih dari
+// < Kurang dari
+// >= Lebih dari atau sama dengan
+// <= Kurang dari atau sama dengan
+
+// == sama dengan
+  print(10 == 10);
+
+// != tidak sama dengan
+  print(10 != 11);
+
+// > lebih dari
+  print(10 > 4);
+
+//< kurang dari
+
+  print(10 < 78);
+// >= lebih dari atau sama dengan
+  print(10 >= 10);
+// <= kurang dari atau sama dengan
+  print(10 <= 10);
+}
