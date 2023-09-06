d6c9be6c0898cb569fe299a0c24692572254d7cb
epul2207 <epuls073@gmail.com>
2023-09-06 20:08:57 +0700

    Menambahkan file operator penugasan

---
+++ operator_penugasan_15.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('Operator Penugasan');
+
+// Operator Penugasan
+// Operator Aritmatika Augmented Assignments
+// a = a + 10 a += 10
+// a = a - 10 a -= 10
+// a = a * 10 a *= 10
+// a = a / 10 a /= 10
+// a = a ~/ 10 a ~/= 10
+// a = a % 10 a %= 10
+}
