4c1d36b0eb36e4c475fee0811f5bbdcf121d8743
epul2207 <epuls073@gmail.com>
2023-09-06 20:27:37 +0700

    Menambahkan file operator logika

---
+++ operator_logika_16.dart
@@ -0,0 +1,41 @@
+void main() {
+  print('Operator logika');
+
+//   Operator Logika
+// ● Operator logika adalah operator untuk dua buah data bool
+// ● Hasil dari operator logika adalah bool lagi
+// Operator Logika
+// Operator Keterangan
+// && Dan
+// || Atau
+// ! Kebalikan
+
+//&& dan
+  var a = true && true;
+  print(a);
+  var b = true && false;
+  print(b);
+  var c = false && true;
+  print(c);
+  var d = false && false;
+  print(d);
+
+  print('==================');
+
+  // || or
+
+  var abang = true || true;
+  print(abang);
+  var ayah = true || false;
+  print(ayah);
+  var ibu = false || true;
+  print(ibu);
+  var anak = false || false;
+  print(anak);
+
+  // !
+  print('====================');
+  var balikan = !true;
+
+  print(balikan);
+}
