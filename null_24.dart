ee06e28726bef5e1a5fc6f2e56511225f87d6de5
epul2207 <epuls073@gmail.com>
2023-09-07 09:48:52 +0700

    Menambahkan data null

---
+++ null_24.dart
@@ -0,0 +1,16 @@
+void main() {
+  print('Null');
+
+//   Null
+// ● Kata kunci null merupakan tipe data representasi data kosong
+// ● Secara default, saat kita membuat variable, data harus diisi, jika tidak diisi, maka variable tidak bisa
+// digunakan
+// ● Namun jika kita ingin membuat sebuah variable bisa diisi data null, kita bisa tambahkan karakter ?
+// (tanda tanya), seperti :
+// TipeData? namaVariable = null;
+
+  int? ini;
+  // ini adalah null;
+
+  print(ini);
+}
