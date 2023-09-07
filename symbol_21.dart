aac2ae1961e317d1c46a76c16737c5e1553b7346
epul2207 <epuls073@gmail.com>
2023-09-07 08:55:47 +0700

    Menambahkan file symbol

---
+++ symbol_21.dart
@@ -0,0 +1,16 @@
+void main() {
+  print('Tipe data Symbol');
+
+//   Symbol
+// ● Symbol merupakan tipe data yang sebenarnya jarang sekali digunakan
+// ● Symbol sebenarnya bisa digunakan sebagai constant
+// ● Untuk membuat Symbol, kita bisa menggunakan tanda #, atau jika kita butuh membuat Symbol
+// dengan nama yang mengandung spasi, kita bisa gunakan Symbol(“text”)
+
+  Symbol tanda = Symbol('Otong Surotong');
+
+  var tanda_kenal = #Marpuah;
+
+  print(tanda);
+  print(tanda_kenal);
+}
