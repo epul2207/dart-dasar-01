b7a4870cecf3af171da34b466016d6e79962f724
epul2207 <epuls073@gmail.com>
2023-09-09 08:18:33 +0700

    Menambahkan fila ternary operator

---
+++ ternaru_operator_25.dart
@@ -0,0 +1,15 @@
+void main() {
+  print('Ternary Operator');
+
+//   Ternary Operator
+// ● Ternary operator adalah operator sederhana dari if
+// ● Ternary operator terdiri dari kondisi yang dievaluasi, jika menghasilkan true maka nilai pertama
+// diambil, jika false, maka nilai kedua diambil
+
+// ?:
+  var nilai = 70;
+
+  var ucapan = nilai >= 75 ? "Lulus" : "Tidak Lulus";
+
+  print(ucapan);
+}
