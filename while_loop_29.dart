827f631fec4854ace6dd155b2a610ef3cb48146b
epul2207 <epuls073@gmail.com>
2023-09-09 08:46:04 +0700

    Menambahkan file while loop

---
+++ while_loop_29.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('While Looping');
+
+//   While Loop
+// ● While loop adalah versi perulangan yang lebih sederhana dibanding for loop
+// ● Di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement
+
+  var nilai = 1;
+
+  while (nilai <= 10) {
+    print('Perulangan ke $nilai');
+    nilai++;
+  }
+}
